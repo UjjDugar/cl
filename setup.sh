51d2298e9c1fbdeaeed6a333a6483cf4df04f5b7
UjjDugar <ujjawaldugar@gmail.com>
2025-02-14 15:50:06 -0800

    new update 2025-02-14

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 pip install huggingface wandb 
 huggingface-cli login --token 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
 wandb login 0a6c8de726025aca725a2bb24178045a0b08cc25
-pip install transformers datasets librosa soundfile accelerate 
+pip install datasets librosa soundfile accelerate 
+pip install torch==2.5.1 
+pip install transformers==4.47.0
 pip install flash_attn==2.7.3
 
 apt update 
