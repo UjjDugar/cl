5b7ea67385f0f3e1e3519a8f802fb927be51a1da
UjjDugar <ujjawaldugar@gmail.com>
2025-02-14 16:03:23 -0800

    new update 2025-02-14

--- download_tts_training_data.py
+++ download_tts_training_data.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "UjjD/tts_dataset_1.05M_unpadded_text"
+repo_id = "UjjD/tts_dataset_1.05M_unpadded_start"
 
 snapshot_download(
     repo_id=repo_id,
