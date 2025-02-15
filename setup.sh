5c866c6ee930e6a83dcf6c38eeb3b99585eb71fc
UjjDugar <ujjawaldugar@gmail.com>
2025-02-15 14:49:22 -0800

    new update 2025-02-15

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt install python3-pip
 export PATH=$HOME/.local/bin:$PATH 
 
 pip install huggingface wandb 
+export PATH=$HOME/.local/bin:$PATH 
 huggingface-cli login --token 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
 wandb login 0a6c8de726025aca725a2bb24178045a0b08cc25
 pip install datasets librosa soundfile accelerate snac torchaudio
--- setupp.sh
+++
@@ -1,2 +0,0 @@
-export PATH=$HOME/.local/bin:$PATH 
-huggingface-cli login --token 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
