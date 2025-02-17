11d0ce336b0eabc1ca50e48c576c3cd3ab6ef1f9
UjjDugar <ujjawaldugar@gmail.com>
2025-02-17 12:37:22 -0800

    new update 2025-02-17

--- training_FSDP.py
+++ training_FSDP.py
@@ -89,12 +89,12 @@ epochs = 1
 batch_size = 4
 
 dataset = load_dataset(dsn, split="train")
-# dataset = dataset.shuffle(seed=42)
+dataset = dataset.shuffle(seed=42)
 
 # Initialize wandb with project name and run name
 wandb.init(
     project="tts-amu",  # Project name in wandb
-    name="ujj-original-unshuffled",  # Name of this training run
+    name="ujj-with_fsdp_trainer",  # Name of this training run
     config={  # Track hyperparameters
         "model_name": model_name,
         "dataset": dsn,
