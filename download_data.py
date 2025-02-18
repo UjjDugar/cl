fb866e911241906077452c0138566b449f350b51
UjjDugar <ujjawaldugar@gmail.com>
2025-02-17 20:23:45 -0800

    new update 2025-02-17

--- download_data.py
+++ download_data.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "UjjD/tts_dataset_1.05M_padded_text_labels_on"
+repo_id = "amuvarma/snac-10m-tts-combined"
 
 snapshot_download(
     repo_id=repo_id,
