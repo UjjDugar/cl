b7d8a433d7917eb53c447317c78ba0216b7f5097
UjjDugar <ujjawaldugar@gmail.com>
2025-02-18 16:09:41 -0800

    new update 2025-02-18

--- download_data.py
+++ download_data.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "UjjD/tts_dataset_1.05M_padded_text_labels_on"
+repo_id = "amuvarma/3b-10m-pretrain-full"
 
 snapshot_download(
     repo_id=repo_id,
